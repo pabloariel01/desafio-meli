d5820ae4c672a3246b9f2f1c93936e59349ab1f3
pablo silva <pabloariel01@hotmail.com>
2020-06-07 21:33:41 -0300

    removed logs, added try catch

--- controllers/items.js
+++ controllers/items.js
@@ -2,26 +2,36 @@ const search = require('../models/search');
 const itemDetails = require('../models/item-details');
 
 exports.getItemById = (req, res, next) => {
-  const itemId = req.params.id;
-  itemDetails(itemId)
+  try{
+
+    const itemId = req.params.id;
+    itemDetails(itemId)
     .then((item) => {
       res.status(200).json(item);
     })
     .catch((error) => {
-      console.log(error);
       res.sendStatus(400)
     });
+  }catch(e){
+    console.log(e)
+    res.sendStatus(400)
+  }
 };
 
 exports.getItems = (req, res, next) => {
-  const query = req.query.q;
-  search(query)
-  .then((result) => {
+  try{
+
+    const query = req.query.q;
+    search(query)
+    .then((result) => {
       res.status(200).json(result);
     })
     .catch((error) => {
-        console.log(error)
-        res.sendStatus(400)
+      console.log(error)
+      res.sendStatus(400)
     });
+  }catch(e){
+    res.sendStatus(400)
+  }
 };
 
